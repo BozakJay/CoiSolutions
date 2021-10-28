5c8084c4122a2d0ab307b362b748b634ec860045
BozakJay <bozakthelad@yahoo.com>
2021-10-28 11:11:18 +0100

    more stuff yo lets go

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,6 @@
+var carousel_images = document.getElementsByClassName('carousel_image');
+var inter = [];
+for(let i=0; i<carousel_images.length; i++){
+  inter.push(carousel_images[i]);
+}
+carousel_images=inter;
